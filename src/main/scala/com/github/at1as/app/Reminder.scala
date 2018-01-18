da4a59da239995225b5130d064c048a115817d98
Jason Willems <jason@willems.ca>
2018-01-17 22:48:37 -0800

    Add futures around http responses

--- src/main/scala/com/github/at1as/app/Reminder.scala
+++ src/main/scala/com/github/at1as/app/Reminder.scala
@@ -2,16 +2,18 @@ package com.github.at1as.app
 
 import java.text.SimpleDateFormat
 import java.util.{Calendar, Locale}
+
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra._
 import org.scalatra.json._
-import com.github.at1as.app.TwilioClient.{initializeTwilio, sendMessage}
+import com.github.at1as.app.TwilioClient.sendMessage
 import com.github.at1as.app.MessageCallback
+
+import scala.concurrent.{ExecutionContext, Future}
 
 class Reminder extends ScalatraServlet with MethodOverride with JacksonJsonSupport {
 
   protected implicit lazy val jsonFormats: Formats = DefaultFormats
-
 
   get("/") {
     views.html.hello()
@@ -48,92 +50,84 @@ class Reminder extends ScalatraServlet with MethodOverride with JacksonJsonSuppo
   }
   
   post("/reminder") {
-    // Twilio only offers a single web-hook for incoming numbers
-    // which must be either GET or POST
-    // so all app logic will be behind this endpoint
-
-    val DAYS = Array(
-      "monday",
-      "tuesday",
-      "wednesday",
-      "thursday",
-      "friday",
-      "saturday",
-      "sunday"
-    )
-
-    val from   = params("From")
-    val action = params("Body").split(" ").head.toLowerCase
-    val text   = params("Body").split(" ").tail.mkString(" ")
-
-    if (Array("unsubscribe", "cancel", "stop", "completed") contains action) {
-
-      // If no reminder ID is passed, delete all reminders for the incoming number
-      var deletedNum: Int = 0
-
-      if (text == "") {
-        deletedNum = Spreadsheet.removeAccountEntries(from)
-      } else {
-        deletedNum = Spreadsheet.remoteEntry(from, text.toInt)
-      }
-
-      initializeTwilio()
-      sendMessage(from, s"Removed $deletedNum reminders")
-
-      //} else if (action contains ("daily", "weekly", "weekdays", "weekends", DAYS : _*)) {
-    } else if (Array("daily", "weekly", "weekdays", "weekends", DAYS).flatMap { case s: String => Array(s); case as: Array[String] => as } contains action) {
-
-      var schedule: Array[String] = Array()
-
-      action match {
-        case "daily" =>
-          schedule = DAYS
-        case "weekly" =>
-          schedule = Array(new SimpleDateFormat("EEEE", Locale.ENGLISH).format(Calendar.getInstance.getTime))
-        case "weekdays" =>
-          schedule = DAYS.filterNot(day => Array("saturday", "sunday") contains day)
-        case "weekends" =>
-          schedule = Array("saturday", "sunday")
-        case _ =>
-          schedule = Array(action)
-      }
-
-      println(schedule)
-
-      val entryId = Spreadsheet.addEntry(
-        from,
-        text,
-        schedule
+    // Twilio only offers oine single web-hook for incoming numbers which must
+    // be either GET or POSTso all app logic will be behind this endpoint
+    new AsyncResult { val is: Unit = {
+      val DAYS = List(
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday"
       )
 
-      initializeTwilio()
-      sendMessage(from, s"You are now subscribed on $action schedule to receive reminders for: $text (id: $entryId)")
-    }
+      val from   = params("From")
+      val action = params("Body").split(" ").head.toLowerCase
+      val text   = params("Body").split(" ").tail.mkString(" ")
+
+      if (List("unsubscribe", "cancel", "stop", "completed") contains action) {
+
+        // If no reminder ID is passed, delete all reminders for the incoming number
+        var deletedNum: Int = 0
+
+        if (text.isEmpty) {
+          deletedNum = Spreadsheet.removeAccountEntries(from)
+        } else {
+          deletedNum = Spreadsheet.removeEntry(from, text.toInt)
+        }
+
+        sendMessage(from, s"Removed $deletedNum reminders")
+
+      } else if (List("daily", "weekly", "weekdays", "weekends", DAYS).flatMap { case s: String => List(s); case as: List[_] => as } contains action) {
+
+        var schedule: List[String] = List()
+
+        action match {
+          case "daily" =>
+            schedule = DAYS
+          case "weekly" =>
+            schedule = List(new SimpleDateFormat("EEEE", Locale.ENGLISH).format(Calendar.getInstance.getTime))
+          case "weekdays" =>
+            schedule = DAYS.filterNot(day => Array("saturday", "sunday") contains day)
+          case "weekends" =>
+            schedule = List("saturday", "sunday")
+          case _ =>
+            schedule = List(action)
+        }
+
+        println(schedule)
+        val entryId = Spreadsheet.addEntry(from, text, schedule)
+
+        sendMessage(from, s"You are now subscribed on $action schedule to receive reminders for: $text (id: $entryId)")
+      }
+    }}
   }
 
   post("/scheduler") {
-    initializeTwilio()
+    new AsyncResult { val is: Unit = {
+      // send all scheduled messages
+      val dateFormat = new SimpleDateFormat("d-M-y")
+      val today = dateFormat.format(Calendar.getInstance().getTime)
+      val dayOfWeek = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(Calendar.getInstance.getTime)
 
-    // send all scheduled messages
-    val dateFormat = new SimpleDateFormat("d-M-y")
-    val today      = dateFormat.format(Calendar.getInstance().getTime)
-    val dayOfWeek  = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(Calendar.getInstance.getTime)
+      var jobIds: Array[Int] = Array()
 
-    var jobIds: Array[Int] = Array()
+      // Find messages scheduled to send on the current day
+      val messages = Spreadsheet.findEntryBySchedule(dayOfWeek)
+      println(f"Batching ${messages.size} reminders to send")
 
-    // Find messages scheduled to send on the current day
-    val messages = Spreadsheet.findEntryBySchedule(dayOfWeek)
-    println(f"Batching ${messages.size} reminders to send")
+      messages.foreach(fields => {
+        sendMessage(
+          fields("TO_NUMBER"), fields("MSG_BODY")
+        )
+        jobIds :+= fields("ID").toInt
+      })
 
-    messages.foreach(fields => {
-      sendMessage(
-        fields("TO_NUMBER"), fields("MSG_BODY")
-      )
-      jobIds :+= fields("ID").toInt
-    })
-
-    // Update "LAST_SENT" field on the job
-    jobIds.foreach(id => Spreadsheet.updateEntryLastSent(id))
+      // Update "LAST_SENT" field on the job
+      jobIds.foreach(id => Spreadsheet.updateEntryLastSent(id))
+    }
   }
 
 }
--- src/main/scala/com/github/at1as/app/TwilioClient.scala
+++ src/main/scala/com/github/at1as/app/TwilioClient.scala
@@ -6,11 +6,12 @@ import com.typesafe.config.{Config, ConfigFactory}
 import com.twilio.rest.api.v2010.account.Message
 
 import java.io.File
+import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success, Try}
 
 object TwilioClient {
 
-  def initializeTwilio(): Unit = {
+  val initializeTwilio: Unit = {
     val conf = getConf
 
     val ACCOUNT_SID = conf.getString("twilio_account.account_sid")
@@ -19,19 +20,25 @@ object TwilioClient {
     Twilio.init(ACCOUNT_SID, AUTH_TOKEN)
   }
 
-  def sendMessage(toNumber: String, body: String): Unit = {
-    val conf = getConf
+  def sendMessage(toNumber: String, body: String)(implicit ctx: ExecutionContext): Future[Unit] = {
+    Future {
+      val conf = getConf
 
-    val from = new PhoneNumber(conf.getString("twilio_account.from_number"))
-    val to   = new PhoneNumber(toNumber)
+      val from = new PhoneNumber(conf.getString("twilio_account.from_number"))
+      val to   = new PhoneNumber(toNumber)
 
-    Try(Message.creator(to, from, body).create()) match {
-      case Success(m) => println(s"Message sent to $to with message SID ${m.getSid}")
-      case Failure(e) => println(s"Encountered exception: \n${e.getMessage}")
+      Try(Message.creator(to, from, body).create()) match {
+        case Success(m) =>
+          val msg = s"Message sent to $to with message SID ${m.getSid}"
+          println(msg)
+
+        case Failure(e) =>
+          println(s"Encountered exception: \n${e.getMessage}")
+      }
     }
   }
 
-  def getConf: Config = {
+  private lazy val getConf: Config = {
     val filename = new java.io.File(".").getCanonicalPath + "/src/main/resources/twilio_account.conf"
     val parsedConfig = ConfigFactory.parseFile(new File(filename))
 
